7c4cf5a004b096b00de54c4d38b742da2411f67b
Cesar Segura <cesar.segura@ucem.ac.cr>
2023-07-01 16:43:23 -0400

    Primer Commit

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$dsn = 'mysql:host=51.222.14.197;dbname=chatabs';
+$user = 'Ch4t4-bD';
+$pass = '23qkiTpaizSqeWlFB6LP';
+$option = array(
+	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+);
+try {
+	$con = new PDO($dsn, $user, $pass, $option);
+	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	//echo 'Good Very Good !';
+} catch (PDOException $ex) {
+	echo "Failed to connect with database ! " . $ex->getMessage();
+	die();
+}
